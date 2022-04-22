324327a587da6b45560f7f3d73b8639aa79df9ae
Zhe Li <zhe.li@bcm.edu>
2022-04-21 23:58:41 -0500

    Rewrite single box environment
    The environment is rewritten as in gym style. Belief environment will be implemented as a wrapper.

--- boxforage/single_box.py
+++ boxforage/single_box.py
@@ -1,36 +1,54 @@
 import numpy as np
+import gym
 from gym.spaces import Discrete, MultiDiscrete
-from irc.distributions import EnergyBasedDistribution as Distribution
+from irc.distributions import BaseDistribution as Distribution
+from irc.distributions import DiscreteDistribution
 from irc.environments import BeliefMDPEnvironment
 
-from typing import Optional, TypeVar
-RandomGenerator = np.random.Generator
+from typing import Optional, Union
+RandGen = np.random.Generator
 
 from jarvis.utils import flatten, nest
 
 
-class SingleBoxForaging(BeliefMDPEnvironment):
+class SingleBoxForaging(gym.Env):
+    r"""Single box foraging environment.
+
+    A minimal example of foraging experiment. Only one box exists and food will
+    appear with fixed probability at each time step if it is not already there.
+    The agent needs to decide whether to open the box or not based on the
+    observed color cue of the box. Monochromatic cues are draw from a binomial
+    distribution with probability p if food exists, and 1-p otherwise.
+
+    """
 
     def __init__(self,
         *,
         env_spec: Optional[dict] = None,
-        belief_class: Optional[TypeVar[Distribution]] = None,
-        rng: Optional[RandomGenerator] = None,
+        rng: Union[RandGen, int, None] = None,
     ):
-        self.env_spec = self._get_env_spec(**env_spec)
-        state_space, action_space, obs_space = self._get_spaces(self.env_spec)
+        r"""
+        Args
+        ----
+        env_spec:
+            Environment specification.
+        rng:
+            Random generator.
 
-        super(SingleBoxForaging, self).__init__(
-            state_space, action_space, obs_space, belief_class,
-            rng=rng,
-            )
+        """
+        self.env_spec = self._get_env_spec(**(env_spec or {}))
+        self.state_space = MultiDiscrete([2]) # box state
+        self.action_space = Discrete(2) # wait and fetch
+        self.observation_space = MultiDiscrete([self.env_spec['box']['num_shades']+1]) # color cue
+
+        self.rng = rng if isinstance(rng, RandGen) else np.random.default_rng(rng)
 
     @staticmethod
     def _get_env_spec(**kwargs):
         r"""Returns full environment specification."""
         env_spec = flatten({
             'box': {
-                'num_grades': 3, 'p_appear': 0.2, 'p_cue': 0.8,
+                'num_shades': 3, 'p_appear': 0.2, 'p_cue': 0.8,
             },
             'reward': {
                 'food': 10., 'fetch': -1.,
@@ -41,35 +59,42 @@ class SingleBoxForaging(BeliefMDPEnvironment):
                 env_spec[key] = val
         return nest(env_spec)
 
-    @staticmethod
-    def _get_spaces(env_spec):
-        r"""Returns gym spaces."""
-        state_space = MultiDiscrete([2]) # box state
-        action_space = Discrete(2) # wait and fetch
-        obs_space = MultiDiscrete([env_spec['box']['num_grades']+1]) # color cue
-        return state_space, action_space, obs_space
+    def get_state(self):
+        state = (self.has_food,)
+        return state
 
-    def transition_step(self, state, action):
+    def set_state(self, state):
+        self.has_food, = state
+
+    def reset(self):
+        self.has_food = 0
+        obs = self.observe_step()
+        return obs
+
+    def step(self, action):
+        reward, done = self.transition_step(action)
+        obs = self.observe_step()
+        info = {'state': self.get_state()}
+        return obs, reward, done, info
+
+    def transition_step(self, action):
         r"""Transition step."""
         reward, done = 0., False
-        has_food, = state
         if action==0: # wait
-            if has_food==0 and self.rng.random()<self.env_spec['box']['p_appear']:
-                has_food = 1
+            if self.has_food==0 and self.rng.random()<self.env_spec['box']['p_appear']:
+                self.has_food = 1
         else: # fetch
             reward += self.env_spec['reward']['fetch']
-            if has_food==1:
+            if self.has_food==1:
                 reward += self.env_spec['reward']['food']
-                has_food = 0
-        next_state = (has_food,)
-        return next_state, reward, done
+                self.has_food = 0
+        return reward, done
 
-    def observe_step(self, state):
+    def observe_step(self):
         r"""Observation step."""
-        has_food, = state
         color = self.rng.binomial(
-            self.env_spec['box']['num_grades'],
-            self.env_spec['box']['p_cue'] if has_food==1 else 1-self.env_spec['box']['p_cue'],
+            self.env_spec['box']['num_shades'],
+            self.env_spec['box']['p_cue'] if self.has_food==1 else 1-self.env_spec['box']['p_cue'],
         )
         obs = (color,)
         return obs
