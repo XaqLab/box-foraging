cdae82a42251c1ce0a7fac4c44594f542b28c9ad
Zhe Li <zhe.li@bcm.edu>
2022-04-23 16:21:16 -0500

    Update single box environment

--- boxforage/single_box.py
+++ boxforage/single_box.py
@@ -1,10 +1,6 @@
 import numpy as np
 import gym
 from gym.spaces import Discrete, MultiDiscrete
-from irc.distributions import BaseDistribution as Distribution
-from irc.distributions import DiscreteDistribution
-from irc.environments import BeliefMDPEnvironment
-
 from typing import Optional, Union
 RandGen = np.random.Generator
 
@@ -14,11 +10,12 @@ from jarvis.utils import flatten, nest
 class SingleBoxForaging(gym.Env):
     r"""Single box foraging environment.
 
-    A minimal example of foraging experiment. Only one box exists and food will
-    appear with fixed probability at each time step if it is not already there.
-    The agent needs to decide whether to open the box or not based on the
-    observed color cue of the box. Monochromatic cues are draw from a binomial
-    distribution with probability p if food exists, and 1-p otherwise.
+    A minimal example of foraging experiment. Food will exist in a single box
+    and the agent needs to decide whether to open the box based on the
+    monochromatic cue outside the box. Food appears with a fixed probability if
+    it does not already exist and the agent does not open the box at this time
+    step. Color cue is drawn from a binomial distribution with probability p if
+    food exists, and 1-p otherwise.
 
     """
 
@@ -42,6 +39,7 @@ class SingleBoxForaging(gym.Env):
         self.observation_space = MultiDiscrete([self.env_spec['box']['num_shades']+1]) # color cue
 
         self.rng = rng if isinstance(rng, RandGen) else np.random.default_rng(rng)
+        self.reset()
 
     @staticmethod
     def _get_env_spec(**kwargs):
@@ -59,11 +57,28 @@ class SingleBoxForaging(gym.Env):
                 env_spec[key] = val
         return nest(env_spec)
 
+    def get_env_param(self):
+        r"""Returns environment parameters."""
+        env_param = (
+            self.env_spec['box']['p_appear'],
+            self.env_spec['box']['p_cue'],
+            self.env_spec['reward']['food'],
+        )
+        return env_param
+
+    def update_env_spec(self, env_param):
+        r"""Updates environment with parameters."""
+        self.env_spec['box']['p_appear'] = env_param[0]
+        self.env_spec['box']['p_cue'] = env_param[1]
+        self.env_spec['reward']['food'] = env_param[2]
+
     def get_state(self):
+        r"""Returns environemtn state."""
         state = (self.has_food,)
         return state
 
     def set_state(self, state):
+        r"""Sets environment state."""
         self.has_food, = state
 
     def reset(self):
@@ -72,13 +87,6 @@ class SingleBoxForaging(gym.Env):
         return obs
 
     def step(self, action):
-        reward, done = self.transition_step(action)
-        obs = self.observe_step()
-        info = {'state': self.get_state()}
-        return obs, reward, done, info
-
-    def transition_step(self, action):
-        r"""Transition step."""
         reward, done = 0., False
         if action==0: # wait
             if self.has_food==0 and self.rng.random()<self.env_spec['box']['p_appear']:
@@ -88,13 +96,12 @@ class SingleBoxForaging(gym.Env):
             if self.has_food==1:
                 reward += self.env_spec['reward']['food']
                 self.has_food = 0
-        return reward, done
 
-    def observe_step(self):
-        r"""Observation step."""
         color = self.rng.binomial(
             self.env_spec['box']['num_shades'],
             self.env_spec['box']['p_cue'] if self.has_food==1 else 1-self.env_spec['box']['p_cue'],
         )
         obs = (color,)
-        return obs
+
+        info = {}
+        return obs, reward, done, info
