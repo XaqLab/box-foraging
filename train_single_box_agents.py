04b96eee8c0c1608fce18211d8d60df0e189bd48
Zhe Li <zhe.li@bcm.edu>
2022-04-29 17:24:10 -0500

    Add batch training script for single box

---
+++ train_single_box_agents.py
@@ -0,0 +1,16 @@
+import random
+from irc.agents import BeliefAgentFamily
+from boxforage.single_box import SingleBoxForaging
+
+if __name__=='__main__':
+    bafam = BeliefAgentFamily(
+        SingleBoxForaging, num_epochs=50,
+    )
+
+    env_params = []
+    for p_appear in [0.05, 0.1, 0.15, 0.2, 0.25, 0.3]:
+        for p_cue in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+            for r_food in [1, 2, 5, 10]:
+                env_params.append((p_appear, p_cue, r_food))
+    random.shuffle(env_params)
+    bafam.train_agents(env_params)
