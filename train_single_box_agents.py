f146665bc00cfae7b57896c19167e0e7f9e2609f
Zhe Li <zhe.li@bcm.edu>
2022-05-04 00:41:51 -0500

    Minor update

--- train_single_box_agents.py
+++ train_single_box_agents.py
@@ -11,7 +11,7 @@ if __name__=='__main__':
     env_params = []
     for p_appear in [0.05, 0.1, 0.15, 0.2, 0.25, 0.3]:
         for p_cue in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-            for r_food in [1, 2, 5, 10]:
+            for r_food in [2, 5, 10]:
                 env_params.append((p_appear, p_cue, r_food))
     random.shuffle(env_params)
     bafam.train_agents(env_params, num_epochs=30, verbose=1)
