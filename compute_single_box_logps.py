4ab24917bfacc916c834751e31ca8883e2119697
Zhe Li <zhe.li@bcm.edu>
2022-05-19 20:57:13 -0500

    Minor fix

--- compute_single_box_logps.py
+++ compute_single_box_logps.py
@@ -6,8 +6,6 @@ from boxforage.single_box import SingleBoxForaging
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--store-dir', default='cache')
-    parser.add_argument('--eval-interval', default=5, type=int)
-    parser.add_argument('--save-interval', default=5, type=int)
     parser.add_argument('--envs-spec-path', default='jsons/single_box_envs.json')
     parser.add_argument('--episode-path', default='cache/episodes/single_box_example.pickle')
     parser.add_argument('--max-seed', default=6, type=int)
@@ -19,7 +17,7 @@ if __name__=='__main__':
     args = parser.parse_args()
 
     time.sleep(random.random()*args.max_wait)
-    bafam = BeliefAgentFamily(SingleBoxForaging)
+    bafam = BeliefAgentFamily(SingleBoxForaging, store_dir=args.store_dir)
 
     with open(args.envs_spec_path, 'r') as f:
         envs_spec = json.load(f)
