dda8b6fa5aa3064279f5274781b28d4fee75d2f6
Zhe Li <zhe.li@bcm.edu>
2022-11-01 14:06:49 -0500

    Update plot functions

--- boxforage/utils.py
+++ boxforage/utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from itertools import product
 
-def plot_single_box_episode(episode, num_shades=None, figsize=(5, 1)):
+def plot_single_box_episode(episode, num_shades=None, figsize=(5, 4)):
     num_steps = episode['num_steps']
     states = episode['states']
     observations = episode['observations']
@@ -13,55 +13,52 @@ def plot_single_box_episode(episode, num_shades=None, figsize=(5, 1)):
     if num_shades is None:
         num_shades = observations.max()
 
-    fig_w, fig_h = figsize
-    aspect = num_steps*fig_h/fig_w
-    figs = []
-
     fig = plt.figure(figsize=figsize)
-    ax = plt.axes([0.05, 0.05, 0.9, 0.9])
+    fig_w, fig_h = figsize
+    height = 0.26
+    gap = (1-3*height)/4
+    aspect = num_steps*fig_h/fig_w*(height/0.9)
+
+    ax = plt.axes([0.05, 3*gap+2*height, 0.9, height])
     h = ax.imshow(
         states.T, aspect=aspect, extent=[-0.5, num_steps+0.5, -0.5, 0.5],
         vmin=0, vmax=1, origin='lower', cmap='coolwarm',
     )
-    cax = plt.axes([0.97, 0.05, 0.03, 0.9])
+    cax = plt.axes([0.97, 3*gap+2*height, 0.03, height])
     cbar = plt.colorbar(h, cax=cax, label='Has food')
     cbar.set_ticks([0, 1])
     cbar.set_ticklabels(['F', 'T'])
     ax.spines['left'].set_visible(False)
     ax.set_xlim([-0.5, num_steps+0.5])
     ax.set_xticks([0, num_steps])
+    ax.set_xticklabels([])
     ax.set_yticks([])
-    ax.set_xlabel('Time')
-    figs.append(fig)
 
-    fig = plt.figure(figsize=figsize)
-    ax = plt.axes([0.05, 0.05, 0.9, 0.9])
+    ax = plt.axes([0.05, 2*gap+height, 0.9, height])
     h = ax.imshow(
         observations.T, aspect=aspect, extent=[-0.5, num_steps+0.5, -0.5, 0.5],
         vmin=0, vmax=num_shades, origin='lower', cmap='coolwarm',
     )
-    cax = plt.axes([0.97, 0.05, 0.03, 0.9])
+    cax = plt.axes([0.97, 2*gap+height, 0.03, height])
     cbar = plt.colorbar(h, cax=cax, label='Color cue')
     cbar.set_ticks([0, num_shades])
     idxs = (actions==1)&(rewards>0)
     h_true = ax.scatter(np.arange(num_steps)[idxs], np.zeros(sum(idxs)), color='magenta', marker='o', s=50)
     idxs = (actions==1)&(rewards<0)
     h_false = ax.scatter(np.arange(num_steps)[idxs], np.zeros(sum(idxs)), color='salmon', marker='x', s=50)
-    ax.legend([h_true, h_false], ['Has food', 'No food'], loc='upper left', fontsize=12)
+    ax.legend([h_true, h_false], ['Hit', 'Miss'], loc='upper left', fontsize=12)
     ax.spines['left'].set_visible(False)
     ax.set_xlim([-0.5, num_steps+0.5])
     ax.set_xticks([0, num_steps])
+    ax.set_xticklabels([])
     ax.set_yticks([])
-    ax.set_xlabel('Time')
-    figs.append(fig)
 
-    fig = plt.figure(figsize=figsize)
-    ax = plt.axes([0.05, 0.05, 0.9, 0.9])
+    ax = plt.axes([0.05, gap, 0.9, height])
     h = ax.imshow(
         probs.T, aspect=aspect, extent=[-0.5, num_steps+0.5, -0.5, 0.5],
         vmin=0, vmax=1, origin='lower', cmap='coolwarm',
     )
-    cax = plt.axes([0.97, 0.05, 0.03, 0.9])
+    cax = plt.axes([0.97, gap, 0.03, height])
     cbar = plt.colorbar(h, cax=cax, label='Belief')
     cbar.set_ticks([0, 1])
     ax.spines['left'].set_visible(False)
@@ -69,10 +66,9 @@ def plot_single_box_episode(episode, num_shades=None, figsize=(5, 1)):
     ax.set_xticks([0, num_steps])
     ax.set_yticks([])
     ax.set_xlabel('Time')
-    figs.append(fig)
-    return figs
+    return fig
 
-def plot_multi_box_episode(episode, num_shades=None, figsize=(5, 1.5)):
+def plot_multi_box_episode(episode, num_shades=None, figsize=(5, 4)):
     num_boxes = episode['states'].shape[1]-1
     num_steps = episode['num_steps']
     states = episode['states']
@@ -85,32 +81,35 @@ def plot_multi_box_episode(episode, num_shades=None, figsize=(5, 1.5)):
     if num_shades is None:
         num_shades = observations.max()
 
+    fig = plt.figure(figsize=figsize)
     fig_w, fig_h = figsize
-    figs = []
+    height = 0.26
+    gap = (1-3*height)/4
 
-    aspect = num_steps/num_boxes*fig_h/fig_w*1.5
-    fig, ax = plt.subplots(figsize=figsize)
+    aspect = num_steps/num_boxes*fig_h/fig_w*(height/0.9)
+    ax = plt.axes([0.05, 3*gap+2*height, 0.9, height])
     h = ax.imshow(
         states[:, :num_boxes].T, aspect=aspect, extent=[-0.5, num_steps+0.5, -0.5, num_boxes-0.5],
         vmin=0, vmax=1, origin='lower', cmap='coolwarm',
-        )
-    cbar = plt.colorbar(h, label='Has food')
+    )
+    cax = plt.axes([0.97, 3*gap+2*height, 0.03, height])
+    cbar = plt.colorbar(h, cax=cax, label='Has food')
     cbar.set_ticks([0, 1])
     cbar.set_ticklabels(['F', 'T'])
     ax.set_xlim([-0.5, num_steps+0.5])
     ax.set_xticks([0, num_steps])
+    ax.set_xticklabels([])
     ax.set_yticks(range(num_boxes))
     ax.set_yticklabels([f'Box {i}' for i in range(num_boxes)])
-    ax.set_xlabel('Time')
-    figs.append(fig)
 
-    aspect = num_steps/(num_boxes+1)*fig_h/fig_w*1.5
-    fig, ax = plt.subplots(figsize=figsize)
+    aspect = num_steps/(num_boxes+1)*fig_h/fig_w*(height/0.9)
+    ax = plt.axes([0.05, 2*gap+height, 0.9, height])
     h = ax.imshow(
         observations[:, :num_boxes].T, aspect=aspect, extent=[-0.5, num_steps+0.5, -0.5, num_boxes-0.5],
         vmin=0, vmax=num_shades, origin='lower', cmap='coolwarm',
-        )
-    cbar = plt.colorbar(h, label='Color cue')
+    )
+    cax = plt.axes([0.97, 2*gap+height, 0.03, height])
+    cbar = plt.colorbar(h, cax=cax, label='Color cue')
     cbar.set_ticks([0, num_shades])
     idxs, = np.nonzero((actions!=num_boxes+1)|(observations[:-1, -1]==num_boxes))
     t = np.arange(num_steps)
@@ -119,31 +118,30 @@ def plot_multi_box_episode(episode, num_shades=None, figsize=(5, 1.5)):
     h_true = ax.scatter(t[idxs], observations[idxs, -1], color='magenta', marker='o', s=50)
     idxs, = np.nonzero((actions==num_boxes+1)&(observations[:-1, -1]<num_boxes)&(rewards<0))
     h_false = ax.scatter(t[idxs], observations[idxs, -1], color='salmon', marker='x', s=50)
-    ax.legend([h_true, h_false], ['Has food', 'No food'], loc='upper left', fontsize=12)
+    ax.legend([h_true, h_false], ['Hit', 'Miss'], loc='upper left', fontsize=12)
     ax.set_xlim([-0.5, num_steps+0.5])
     ax.set_ylim([-0.5, num_boxes+0.5])
     ax.set_xticks([0, num_steps])
+    ax.set_xticklabels([])
     ax.set_yticks(range(num_boxes+1))
     ax.set_yticklabels([f'Box {i}' for i in range(num_boxes)]+['Center'])
-    ax.set_xlabel('Time')
-    figs.append(fig)
 
     probs = np.zeros((num_steps+1, num_boxes))
     for b_idx in range(num_boxes):
         idxs = has_foods[:, b_idx]==1
         probs[:, b_idx] = q_probs[:, idxs].sum(axis=1)
-    aspect = num_steps/num_boxes*fig_h/fig_w*1.5
-    fig, ax = plt.subplots(figsize=figsize)
+    aspect = num_steps/num_boxes*fig_h/fig_w*(height/0.9)
+    ax = plt.axes([0.05, gap, 0.9, height])
     h = ax.imshow(
         probs.T, aspect=aspect, extent=[-0.5, num_steps+0.5, -0.5, num_boxes-0.5],
         vmin=0, vmax=1, origin='lower', cmap='coolwarm',
     )
-    cbar = plt.colorbar(h, label='Belief')
+    cax = plt.axes([0.97, gap, 0.03, height])
+    cbar = plt.colorbar(h, cax=cax, label='Belief')
     cbar.set_ticks([0, 1])
     ax.set_xlim([-0.5, num_steps+0.5])
     ax.set_xticks([0, num_steps])
     ax.set_yticks(range(num_boxes))
     ax.set_yticklabels([f'Box {i}' for i in range(num_boxes)])
     ax.set_xlabel('Time')
-    figs.append(fig)
-    return figs
+    return fig
