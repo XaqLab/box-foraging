41294c7614dba52c734117e1464027b00e0bce0d
Zhe Li <zhel@bcm.edu>
2022-12-29 16:04:16 -0600

    Update sweep script

--- demo-sweep.py
+++ demo-sweep.py
@@ -2,27 +2,28 @@ import yaml
 from jarvis.config import Config, from_cli
 from irc.manager import IRCManager
 
-config = Config({
-    'env_param_grid': 'param_grids/indie_boxes.yaml',
+cli_args = Config({
+    'env_param_grid': 'param_grids/single_box.yaml',
     'seeds': None,
     'num_epochs': 12,
     'count': 1,
     'patience': 4.,
-    'defaults': 'defaults/indie_boxes.yaml',
+    'defaults': 'irc_defaults/single_box.yaml',
 })
 
 if __name__=='__main__':
-    config.update(from_cli())
-    env_param_grid = config.pop('env_param_grid')
+    cli_args.update(from_cli())
+    env_param_grid = cli_args.pop('env_param_grid')
     if isinstance(env_param_grid, str):
         with open(env_param_grid) as f:
+            print("Environment parameter grid loaded from {env_param_grid}")
             env_param_grid = yaml.safe_load(f)
-    seeds = config.pop('seeds')
-    num_epochs = config.pop('num_epochs')
-    count = config.pop('count')
-    patience = config.pop('patience')
+    seeds = cli_args.pop('seeds')
+    num_epochs = cli_args.pop('num_epochs')
+    count = cli_args.pop('count')
+    patience = cli_args.pop('patience')
 
-    manager = IRCManager(**config)
+    manager = IRCManager(**cli_args)
     manager.train_agents(
         env_param_grid=env_param_grid, seeds=seeds,
         num_epochs=num_epochs, count=count, patience=patience,
