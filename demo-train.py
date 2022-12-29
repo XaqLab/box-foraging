786ab8ef184bcd98abf757410c80ba8b04c34700
Zhe Li <zhel@bcm.edu>
2022-12-29 16:16:44 -0600

    Update train script

--- demo-train.py
+++ demo-train.py
@@ -1,17 +1,17 @@
 from jarvis.config import Config, from_cli
 from irc.manager import IRCManager
 
-config = Config({
-    'env_param': None, 'seed': None, 'num_epochs': 12,
-    'defaults': 'defaults/single_box.yaml',
-    'device': 'cpu',
+cli_args = Config({
+    'env_param': None, 'seed': None,
+    'num_epochs': 12,
+    'defaults': 'irc_defaults/single_box.yaml',
 })
 
 if __name__=='__main__':
-    config.update(from_cli())
-    env_param = config.pop('env_param')
-    seed = config.pop('seed')
-    num_epochs = config.pop('num_epochs')
+    cli_args.update(from_cli())
+    env_param = cli_args.pop('env_param')
+    seed = cli_args.pop('seed')
+    num_epochs = cli_args.pop('num_epochs')
 
-    manager = IRCManager(**config)
+    manager = IRCManager(**cli_args)
     manager.train_agent(env_param, seed, num_epochs=num_epochs)
