62c265d6f2b6a45baf6087da82a8e7c1b1d5ac7a
Zhe Li <zhe.li@bcm.edu>
2023-02-25 16:14:21 -0600

    Update scripts

--- sweep-agent-grid.py
+++ sweep-agent-grid.py
@@ -2,29 +2,17 @@ import yaml
 from jarvis.config import Config, from_cli
 from irc.manager import IRCManager
 
-args = Config({
+cli_args = Config({
     'store_dir': 'irc_store',
     'agent_defaults': 'irc_defaults/identical_boxes.yaml',
     'env_param_grid': 'param_grids/identical_boxes.yaml',
     'seeds': None,
-    'num_epochs': 12,
-    'count': 1,
-    'patience': 4.,
 })
 
 if __name__=='__main__':
-    args.update(from_cli())
+    cli_args.update(from_cli())
     manager = IRCManager(
-        store_dir=args.store_dir,
-        agent_defaults=args.agent_defaults,
+        store_dir=cli_args.pop('store_dir'),
+        agent_defaults=cli_args.pop('agent_defaults'),
     )
-
-    env_param_grid = args.env_param_grid
-    if isinstance(env_param_grid, str):
-        with open(env_param_grid) as f:
-            print(f"Environment parameter grid loaded from '{env_param_grid}'.")
-            env_param_grid = yaml.safe_load(f)
-    manager.train_agents(
-        env_param_grid=env_param_grid, seeds=args.seeds,
-        num_epochs=args.num_epochs, count=args.count, patience=args.patience,
-    )
+    manager.train_agents(**cli_args)
--- train-one-agent.py
+++ train-one-agent.py
@@ -1,20 +1,16 @@
 from jarvis.config import Config, from_cli
 from irc.manager import IRCManager
 
-args = Config({
+cli_args = Config({
     'store_dir': 'irc_store',
     'agent_defaults': 'irc_defaults/identical_boxes.yaml',
     'env_param': None,
-    'seed': None,
-    'num_epochs': 30,
 })
 
 if __name__=='__main__':
-    args.update(from_cli())
+    cli_args.update(from_cli())
     manager = IRCManager(
-        store_dir=args.store_dir,
-        agent_defaults=args.agent_defaults,
+        store_dir=cli_args.pop('store_dir'),
+        agent_defaults=cli_args.pop('agent_defaults'),
     )
-    manager.train_agent(
-        args.env_param, args.seed, args.num_epochs,
-    )
+    manager.train_agent(**cli_args)
